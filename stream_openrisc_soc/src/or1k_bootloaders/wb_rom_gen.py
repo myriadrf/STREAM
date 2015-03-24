08016884c5c00d025cf32806c6385959d92aea7e
Olof Kindgren <olof.kindgren@gmail.com>
2015-03-24 15:13:21 +0100

    Fix license headers for or1k_bootloaders

--- stream_openrisc_soc/src/or1k_bootloaders/wb_rom_gen.py
+++ stream_openrisc_soc/src/or1k_bootloaders/wb_rom_gen.py
@@ -1,3 +1,19 @@
+# wb_rom_gen.py -- Convert bin files to verilog readmemh files
+#
+# Copyright (c) 2014, 2015 Authors
+#
+# Contributor Olof Kindgren <olof.kindgren@gmail.com>
+#
+# The authors hereby grant permission to use, copy, modify, distribute,
+# and license this software and its documentation for any purpose, provided
+# that existing copyright notices are retained in all copies and that this
+# notice is included verbatim in any distributions. No written agreement,
+# license, or royalty fee is required for any of the authorized uses.
+# Modifications to this software may be copyrighted by their authors
+# and need not follow the licensing terms described here, provided that
+# the new terms are clearly indicated on the first page of each file where
+# they apply.
+#
 from struct import unpack
 import sys
 infile = sys.argv[1]
